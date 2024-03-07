81959c64c3a2af1f1c49f5a3be73717daeffdacb
viveroa2291 <viveroa2291@uwec.edu>
2024-03-06 21:06:11 -0600

    Trying to get the login button to work

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,16 @@
 import React from "react";
+import {Nav} from "react-bootstrap";
 import logo from './images/logo.png';
 import './navbar.css';
 function Navbar () {
     return (
         <div className="navbar-body">
-            <img className="logo" src={logo} alt="This will be the main logo"/>
+            <span>
+                <img className="logo" src={logo} alt="This will be the main logo"/>
+            </span>
+            <div>
+                <Nav.Link className="login-button" to="/Login">Login</Nav.Link>
+            </div>
         </div>
     )
 }
---
+++ src/components/Login.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Login() {
+    return(
+        <div>
+            <p>Slava Ukraine</p>
+        </div>
+    )
+}
+export default Login;
