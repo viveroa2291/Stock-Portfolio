cdc7db2bd3d78d22814cda0be4a641ff12b952fa
viveroa2291 <viveroa2291@uwec.edu>
2024-03-09 21:29:44 -0600

    Adding my about page

---
+++ src/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './about.css';
+function About () {
+    return(
+        <div>
+            <h1 className="about-us-h1">About Us</h1>
+            <h2 className="about-us-h2">The best stock portofolio tool.</h2>
+        </div>
+    )
+}
+export default About;
--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import {Route, Switch} from "react-router-dom";
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Home from './Home';
+import About from './About';
 function App() {
   return (
     <Switch>
       <Route path="/login" component={Login}/>
       <Route path="/signup" component={Signup}/>
+      <Route path="/about" component={About}/>
       <Home/>
     </Switch>
   );
