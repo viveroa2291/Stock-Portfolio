a8d2b3513dc6cc8132b72932375b1ce0f06b986d
viveroa2291 <viveroa2291@uwec.edu>
2024-03-10 21:42:47 -0500

    Added some stuff for the signup page

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,9 +1,26 @@
 import React from "react";
-
+import '../CSS/signup.css';
 function Signup() {
     return (
         <div>
-            <p>Signup</p>
+            <form>
+                <label>First Name:</label>
+                <br/>
+                <input></input>
+                <br/>
+                <label>Last Name:</label>
+                <br/>
+                <input></input>
+                <br/>
+                <label>username:</label>
+                <br/>
+                <input></input>
+                <br/>
+                <label>Email Address:</label>
+                <br/>
+                <input></input>
+                <br/>
+            </form>
         </div>
     )
 }
