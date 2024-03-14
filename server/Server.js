9170e2515702be4e6eaaf07fb5ccb6c2d853bc32
viveroa2291 <viveroa2291@uwec.edu>
2024-03-14 18:56:33 -0500

    Connected to the database

--- server/Server.js
+++ server/Server.js
@@ -4,20 +4,23 @@ const mysql = require('mysql');
 const cors = require('cors');
 
 const app = express();
-const port = 5000;
+const port = 3306;
 
 app.use(cors());
 
-/*
-app.get('/', (req, res) => {
-  res.send('Hello from the Node.js backend!');
+const db = mysql.createConnection({
+  host: 'login-credentials.ckscb41dv4ih.us-east-2.rds.amazonaws.com',
+  user: 'admin',
+  password: 'numberst',
+  database: 'login_credentials'
+})
+db.connect((err) => {
+  if(err) {
+    console.error('Error connecting to the database: ', err);
+    return;
+  }
+  console.log("Connected to the database");
 });
-
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
-*/
-
-app.listen(8801, () => {
-  console.log("Listening...");
-})
