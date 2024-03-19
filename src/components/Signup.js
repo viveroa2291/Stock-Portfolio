dbc9a5db495475d8d2ad6496725c1367214332fa
viveroa2291 <viveroa2291@uwec.edu>
2024-03-19 02:24:17 -0500

    Signup Page Finished

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,27 +1,29 @@
 import React from "react";
+import {Link } from 'react-router-dom';
 import '../CSS/signup.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function Signup() {
     return (
         <div>
-            <form>
-                <label>First Name:</label><label>Last Name:</label>
-                <span className="signup-names">
-                    <br/> 
-                    <input></input>
-                  
-                    <br/>
-                    <input></input> 
-                </span>
-                <br/>
-                <label>Username:</label>
-                <br/>
-                <input></input>
-                <br/>
-                <label>Email Address:</label>
-                <br/>
-                <input></input>
-                <br/>
-            </form>
+            <div className="signup-container">
+                <form>
+                    <div className="heading">
+                        <label for='fname'>First Name:</label> 
+                        <input id="fname"/>                          
+                        <label for='lname'>Last Name:</label>
+                        <input id="lname"/> 
+                        <label for='username'>Username:</label>
+                        <input id="username"/>
+                        <label for='email'>Email Address:</label>
+                        <input id="email"/>
+                    </div>
+                </form>
+                <button className="btn btn-dark create-account">Create An Account</button>
+                <div className="login-section">
+                    <p>Already Have An Account?</p>                
+                    <Link className="btn btn-primary login-button" to="/Login">Login Here</Link>
+                </div>
+            </div>        
         </div>
     )
 }
