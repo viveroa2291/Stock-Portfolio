3256ce04894b535d21d24fcd43dd4d5acd3dbdc5
viveroa2291 <viveroa2291@uwec.edu>
2024-03-20 17:53:39 -0500

    Added the signup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,24 +1,62 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link } from 'react-router-dom';
 import '../CSS/signup.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 function Signup() {
+    // Setting the variables to empty and are being linked with the name attribute in the <input> tag.
+    const[formData, setFormData] = useState({
+        fname: '', lname: '', username: '', email: '', password: '', cpassword: ''
+    });
+
+    const handleInputChange = (e) => {
+        const {name, value} = e.target;
+        setFormData({...formData, [name]:value})
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if(formData.password !== formData.cpassword) {
+            alert("Password do not match");
+            return;
+        }
+        try { 
+            const response = await fetch('http://localhost:3306/signup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            });
+            if(response.ok) {
+                console.log('Signup Successful');
+            }
+            else {
+                console.error('Signup Failed');
+            }
+        } catch (error) {
+            console.error('Error: ', error);
+        }
+    };
     return (
         <div>
             <div className="signup-container">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="heading">
                         <label for='fname'>First Name:</label> 
-                        <input id="fname"/>                          
+                        <input id="fname" name="fname" onChange={handleInputChange} />                          
                         <label for='lname'>Last Name:</label>
-                        <input id="lname"/> 
+                        <input id="lname" name="lname" onChange={handleInputChange}/> 
                         <label for='username'>Username:</label>
-                        <input id="username"/>
+                        <input id="username" name="username" onChange={handleInputChange}/>
                         <label for='email'>Email Address:</label>
-                        <input id="email"/>
+                        <input id="email" name="email" onChange={handleInputChange}/>
+                        <label for='password'>Password:</label>
+                        <input id="password" name="password" onChange={handleInputChange}/>
+                        <label for='cpassword'>Confirm Password:</label>
+                        <input id="cpassword" name="cpassword" onChange={handleInputChange}/>
                     </div>
                 </form>
-                <button className="btn btn-dark create-account">Create An Account</button>
+                <button type="submit" className="btn btn-dark create-account">Create An Account</button>
                 <div className="login-section">
                     <p>Already Have An Account?</p>                
                     <Link className="btn btn-primary login-button" to="/Login">Login Here</Link>
