7202936e3d9b4c5df71b83276f8f2098be418947
viveroa2291 <viveroa2291@uwec.edu>
2024-03-21 02:13:40 -0500

    signup success

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -21,7 +21,8 @@ function Signup() {
             return;
         }
         try { 
-            const response = await fetch('http://localhost:3306/signup', {
+            // Await is used to pause the execution of the code until the 'fetch' operation is completed and a response is received from the server
+            const response = await fetch('http://localhost:3306/Signup', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
@@ -43,21 +44,21 @@ function Signup() {
             <div className="signup-container">
                 <form onSubmit={handleSubmit}>
                     <div className="heading">
-                        <label for='fname'>First Name:</label> 
+                        <label htmlFor='fname'>First Name:</label> 
                         <input id="fname" name="fname" onChange={handleInputChange} />                          
-                        <label for='lname'>Last Name:</label>
+                        <label htmlFor='lname'>Last Name:</label>
                         <input id="lname" name="lname" onChange={handleInputChange}/> 
-                        <label for='username'>Username:</label>
+                        <label htmlFor='username'>Username:</label>
                         <input id="username" name="username" onChange={handleInputChange}/>
-                        <label for='email'>Email Address:</label>
+                        <label htmlFor='email'>Email Address:</label>
                         <input id="email" name="email" onChange={handleInputChange}/>
-                        <label for='password'>Password:</label>
+                        <label htmlFor='password'>Password:</label>
                         <input id="password" name="password" onChange={handleInputChange}/>
-                        <label for='cpassword'>Confirm Password:</label>
+                        <label htmlFor='cpassword'>Confirm Password:</label>
                         <input id="cpassword" name="cpassword" onChange={handleInputChange}/>
-                    </div>
+                    </div>                
+                    <button type="submit" className="btn btn-dark create-account">Create An Account</button>
                 </form>
-                <button type="submit" className="btn btn-dark create-account">Create An Account</button>
                 <div className="login-section">
                     <p>Already Have An Account?</p>                
                     <Link className="btn btn-primary login-button" to="/Login">Login Here</Link>
