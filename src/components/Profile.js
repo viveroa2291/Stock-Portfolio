933f0a2395e61e8d85b9dc938ded525de1b486b1
viveroa2291 <viveroa2291@uwec.edu>
2024-03-25 23:33:40 -0500

    Adding Content

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -16,15 +16,31 @@ function Profile({ isAuthenticated }) {
             <div className='stock-table'>
                 <table>
                     <tr>
-                        <th>Stocks</th>
-                        <th>Buying Price</th>
+                        <th>Stock</th>
+                        <th>Symbol</th>
+                        <th>Shares</th>
+                        <th>Average Cost</th>
                         <th>Price</th>
                         <th>Stock Growth</th>
                     </tr>
                     <tr>
                         <td>Apple</td>
-                        <td>Microsoft</td>                        
+                        <td>AAPL</td>
+                        <td className='shares'>2</td>
+                        <td>54.99</td>
+                    </tr>
+                    <tr>
+                        <td>JP Morgan Chase</td>
                         <td>JPM</td>
+                        <td className='shares'>1</td>
+                    </tr>
+                    <tr>
+                        <td>Microsoft</td> 
+                        <td>MSFT</td>             
+                        <td className='shares'>1</td>          
+                    </tr>
+                    <tr>
+
                     </tr>
                 </table>
             </div>
