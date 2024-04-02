51e8266fdc9942e8e8554a7883fdb255dfd7139e
viveroa2291 <viveroa2291@uwec.edu>
2024-04-02 02:38:35 -0500

    Fixed some UX with the search bar

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -6,6 +6,7 @@ function Explore() {
     const [selectedTicker, setSelectedTicker] = useState(null);
     const [selectedStock, setSelectedStock] = useState(null);
     const [showStockData, setShowStockData] = useState(false);
+    const [searchBarFocused, setSearchBarFocused] = useState(false);
     useEffect(()  => {
         fetch('https://api.polygon.io/v3/reference/dividends?ticker=ORC&apiKey=1PLJT6aBkJqCXlmfbX6lqa0bp6WzEHTK')
         .then(response => response.json()) 
@@ -36,7 +37,6 @@ function Explore() {
             yesterday.setDate(yesterday.getDate() - 2); 
 
             const formattedYesterday = yesterday.toISOString().split('T')[0];
-            console.log("Yesterdays Date: " + formattedYesterday);
             // const response = await fetch(`https://api.polygon.io/v2/aggs/ticker/${ticker}/range/1/day/${formattedYesterday}/${formattedYesterday}?adjusted=true&sort=asc&limit=1&apiKey=1PLJT6aBkJqCXlmfbX6lqa0bp6WzEHTK`);
             const response = await fetch(`https://api.polygon.io/v2/aggs/ticker/${ticker}/range/1/day/2024-03-28/2024-03-28?adjusted=true&sort=asc&limit=1&apiKey=1PLJT6aBkJqCXlmfbX6lqa0bp6WzEHTK`); 
             const stockData = await response.json();
@@ -46,24 +46,27 @@ function Explore() {
         } catch (error) {
             console.error('Error fetching the stock Data: ', error);
         };
-
     }        
     const handleSearchFocus = () => {
         setShowStockData(false); // Hide stock data div when search bar is focused
+        setSearchBarFocused(true);
     }
     const handleSearchBlur = () => {
-        setShowStockData(true); // Show stock data div when search bar loses focus
+        if(!searchQuery) {
+            setShowStockData(true); // Show stock data div when search bar loses focus
+        }
+        setSearchBarFocused(false);
     }
     return (
         <div>
             <p className='welcome-explore'>Welcome to the Explore page.</p>
             <input className='search-bar' type='text' placeholder='Search Stock...' value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} onFocus={handleSearchFocus} onBlur={handleSearchBlur} />
-            {searchQuery && filterData.length > 0 ? (
+            { searchBarFocused && searchQuery && filterData.length > 0 ? (
                 filterData.map((item) => (
-                <p className='stock-ticker' key={item.T} onClick={() => handleClick(item.T)} style={{margin: '0', display: 'block', marginLeft: 'auto', marginRight: 'auto'}}> {item.T}</p>
+                <p className='stock-ticker' key={item.T} onClick={() => handleClick(item.T)} style={{margin: '0', display: 'block', marginLeft: 'auto', marginRight: 'auto'}}>{item.T}</p>
             ))
         ) : null }
-        { showStockData && selectedTicker && (
+        { searchQuery && showStockData && selectedTicker && (
             <div className='stock-data'>
                 <p >Ticker: {selectedTicker.ticker}</p>        
                 { selectedStock && (
@@ -74,7 +77,6 @@ function Explore() {
                     <p>Low Price: {selectedStock.l}</p>
                     </>
                 )}     
-
             </div>
         )}
         </div>
