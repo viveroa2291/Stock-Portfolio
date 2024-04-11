004dd81c16082fd7493f661ed94c9f4a2ef47ba2
viveroa2291 <viveroa2291@uwec.edu>
2024-04-10 21:02:07 -0500

    Added some content to the Settings page

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import { Redirect} from 'react-router-dom';
-
+import '../CSS/settings.css';
 function Settings ({isAuthenticated, firstName, lastName, username}) {
     if(!isAuthenticated) {
         return <Redirect to="/login" />;  
     }
     return(
-        <div>
-            <p>Settings page coming soon</p>
+        <div >
+            <div className='settings-header'>
+                <p>Profile</p>
+            </div>
+            <div className='settings-container'>
+                <label className='settings-label'>First Name</label>                         
+                <input className='settings-input'></input>       
+                <label className='settings-label'>Last Name</label>                    
+                <input className='settings-input'></input>
+                <label className='settings-label'>Email Address</label>
+                <input className='settings-input'></input>
+                <h2>Security</h2>
+                <label className='settings-label'>Password</label>                
+                <input className='settings-input'></input>                
+                <label className='settings-label'>Change Password</label>
+                <input className='settings-input'></input>
+                <button>Save</button>
+            </div>
         </div>
     )
 }
