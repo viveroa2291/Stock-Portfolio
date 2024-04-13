b98626793e140e74ab4c14f8bb275bf799bdae80
viveroa2291 <viveroa2291@uwec.edu>
2024-04-12 22:10:02 -0500

    Added some stuff to the settings page

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -11,17 +11,20 @@ function Settings ({isAuthenticated, firstName, lastName, username}) {
                 <p>Profile</p>
             </div>
             <div className='settings-container'>
+
                 <label className='settings-label'>First Name</label>                         
-                <input className='settings-input'></input>       
+                <input className='settings-input' type="text" name="firstName" placeholder={firstName}/>  
                 <label className='settings-label'>Last Name</label>                    
-                <input className='settings-input'></input>
+                <input className='settings-input' type='text' name="lastName" placeholder={lastName}/> 
                 <label className='settings-label'>Email Address</label>
-                <input className='settings-input'></input>
-                <h2>Security</h2>
+                <input className='settings-input'/>
+                <h2 className='security-header'>Security</h2>                
+                <label className='settings-label'>Username: </label>
+                <input className='settings-input' type="text" name="username" value={username}/>
                 <label className='settings-label'>Password</label>                
-                <input className='settings-input'></input>                
+                <input className='settings-input'/>             
                 <label className='settings-label'>Change Password</label>
-                <input className='settings-input'></input>
+                <input className='settings-input'/>
                 <button className='btn btn-dark setting-save-button btn-primary'>Save</button>
             </div>
         </div>
